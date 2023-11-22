e29075b51802db572a7f54531d25a22dd515a945
SomaanIqbal <isomaan@gmail.com>
2023-11-22 12:06:25 -0800

    singeBooks are now working with the click function

--- Books.jsx
+++ Books.jsx
@@ -1,8 +1,8 @@
 /* TODO - add your code to create a functional React component that displays all of the available books in the library's catalog. Fetch the book data from the provided API. Users should be able to click on an individual book to navigate to the SingleBook component and view its details. */
 import { useState, useEffect } from "react";
-// import BooksRow from "./BooksRow";
 
-const Books = ({}) => {
+
+const Books = ({setSelectedBookId}) => {
   const [books, setBooks] = useState(null);
 
   useEffect(() => {
@@ -22,7 +22,7 @@ const Books = ({}) => {
     <div className="books">
       <ul>
         {books && books.map((book) => {
-          return <li key={book.id}>{book.title}</li>
+          return <li key={book.id} onClick={() => setSelectedBookId(book.id)}>{book.title}</li>
           
         })}
       </ul>
--- SingleBook.jsx
+++ SingleBook.jsx
@@ -1,15 +1,32 @@
 /* TODO - add your code to create a functional React component that renders details for a single book. Fetch the book data from the provided API. You may consider conditionally rendering a 'Checkout' button for logged in users. */
+import { useState, useEffect } from "react";
 
-const SingleBook = () => {
+const SingleBook = ({ selectedBookId, setSelectedBookId }) => {
 
+    const [bookDetails,setBookDetails] = useState(null);
 
-    return (
-        <div className='singleBook'>
-            <h1>Single Book</h1>
-        </div>
-    )
+    useEffect(() => {
+        const fetchBookDetails = async() => {
+            const response = await fetch(`https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books/${selectedBookId}`);
+            const jsonResponse = await response.json();
+            console.log(jsonResponse.book);
+            setBookDetails(jsonResponse.book)
+        }
+        fetchBookDetails()
+    },[])
 
-    
-}
+  return (
+    <div className="singleBook">
+      <ul>
+        {bookDetails? (<div><li>Title: {bookDetails.title}</li> <li>Author{bookDetails.author}</li> <li>Description: {bookDetails.description}</li> <li>Available: {bookDetails.available}</li> <img  src={bookDetails.coverimage}/></div>):(<h2>loading...</h2>)}
+      </ul>
 
-export default SingleBook
+      <button onClick={() => {setSelectedBookId(null)}}>
+        Back
+      </button>
+
+    </div>
+  );
+};
+
+export default SingleBook;
